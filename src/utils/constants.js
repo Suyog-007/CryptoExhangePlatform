bd724ef4769c126ebe93e844f5c1a146e3b0d47b
Suyog-007 <suyogshahb701@gmail.com>
2022-11-22 10:26:20 +0530

    Added stuff

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+import abi from "./Transactions.json";
+
+export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
+export const contractABI = abi.abi;
+
+//0xBC719715c1b95384bcF38BaD60437f08aa816A98
